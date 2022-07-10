105d335d3d696f4d196f081c55ae8a906b69b9fc
Srini555 <srinidupati@gmail.com>
2022-07-10 15:38:06 +0530

    added  Employee class

---
+++ Emplopyee.java
@@ -0,0 +1,13 @@
+public class Emplopyee {
+    
+    private String name;
+    private String id;
+
+    public String getNaem() {
+        return name;
+    }
+    
+    public String getId() {
+        return id;
+    }
+}
